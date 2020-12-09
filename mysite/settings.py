5c2caeb8341dce811a09899752b62cbe6a100420
vvilliam <wpalin@gmail.com>
2020-12-09 10:20:34 -0500

    style(site): Black code reformat

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,9 +9,7 @@ This results in the following concept:
 import glob
 import os
 
-conf_files = glob.glob(
-    os.path.join(os.path.dirname(__file__), "../settings", "*.py")
-)
+conf_files = glob.glob(os.path.join(os.path.dirname(__file__), "../settings", "*.py"))
 conf_files.sort()
 for f in conf_files:
     exec(compile(open(os.path.abspath(f), "rb").read(), os.path.abspath(f), "exec"))
